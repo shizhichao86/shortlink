26c456387b656cd1e5500822b57dc667549079bf
shizhichao <1344811083@qq.com>
2026-03-06 23:00:15 +0800

    feat(回收站): 添加回收站短链接分页查询功能
    - 在回收站服务层添加分页查询方法
    - 实现项目端回收站分页查询接口
    - 在管理端添加远程调用回收站分页接口
    - 在管理端控制器暴露回收站分页查询API

--- admin/src/main/java/com/nageoffer/shortlink/admin/remote/ShortLinkRemoteService.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/remote/ShortLinkRemoteService.java
@@ -30,7 +30,8 @@ public interface ShortLinkRemoteService {
      * @return 短链接创建响应
      */
     default Result<ShortLinkCreateRespDTO> createShortLink(ShortLinkCreateReqDTO requestParam) {
-        String resultBodyStr = HttpUtil.post("http://127.0.0.1:8001/api/short-link/v1/create", JSON.toJSONString(requestParam));
+        String resultBodyStr = HttpUtil.post("http://127.0.0.1:8001/api/short-link/v1/create",
+                JSON.toJSONString(requestParam));
         return JSON.parseObject(resultBodyStr, new TypeReference<>() {
         });
     }
@@ -93,5 +94,21 @@ public interface ShortLinkRemoteService {
      */
     default void saveRecycleBin(RecycleBinSaveReqDTO requestParam) {
         HttpUtil.post("http://127.0.0.1:8001/api/short-link/v1/recycle-bin/save", JSON.toJSONString(requestParam));
+    }
+
+    /**
+     * 分页查询回收站短链接
+     *
+     * @param requestParam 分页短链接请求参数
+     * @return 查询短链接响应
+     */
+    default Result<IPage<ShortLinkPageRespDTO>> pageRecycleBinShortLink(ShortLinkPageReqDTO requestParam) {
+        Map<String, Object> requestMap = new HashMap<>();
+        requestMap.put("gid", requestParam.getGid());
+        requestMap.put("current", requestParam.getCurrent());
+        requestMap.put("size", requestParam.getSize());
+        String resultPageStr = HttpUtil.get("http://127.0.0.1:8001/api/short-link/v1/recycle-bin/page", requestMap);
+        return JSON.parseObject(resultPageStr, new TypeReference<>() {
+        });
     }
 }
